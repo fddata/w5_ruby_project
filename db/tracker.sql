60115bc7122e8d6d71604b25a3cc82653fdd435d
fddata <fraserjdouglas@gmail.com>
2018-08-10 14:32:57 +0100

    adds sql_runner.rb and tracker.sql files

---
+++ db/tracker.sql
@@ -0,0 +1,28 @@
+DROP TABLE transactions; -- Drop the 'many' table first
+DROP TABLE merchants;
+DROP TABLE categories;
+DROP TABLE users;
+
+--Create the 'one' table first, then the 'many' tables last.
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  budget FLOAT
+);
+
+CREATE TABLE merchants(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE categories(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE transactions(
+  id SERIAL PRIMARY KEY,
+  amount FLOAT,
+  time_stamp TIMESTAMP,
+  user_id INT REFERENCES users(id) ON DELETE CASCADE,
+  merchant_id INT REFERENCES merchants(id) ON DELETE CASCADE,
+  category_id INT REFERENCES categories(id) ON DELETE CASCADE);
