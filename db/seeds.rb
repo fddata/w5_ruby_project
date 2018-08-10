7283ca94923ed4f21b75ad6413b29f44f77abce7
fddata <fraserjdouglas@gmail.com>
2018-08-10 15:13:25 +0100

    adds CRUD for Merchant class

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 require_relative '../models/category.rb'
+require_relative '../models/merchant.rb'
 
 require 'pry-byebug'
 
 Category.delete_all()
+Merchant.delete_all()
 
 #Seeding some categories:
 
@@ -22,15 +24,47 @@ category_bills = Category.new({
   'name' => 'bills'
     })
 
+category_gifts = Category.new({
+      'name' => 'gifts'
+        })
+
 category_bills.save()
 category_entertainment.save()
 category_groceries.save()
 category_transport.save()
+category_gifts.save()
+
+
+#Seeding some merchants
+
+merchant_tesco = Merchant.new({
+  'name' => 'Tesco'
+  })
+
+merchant_cineworld = Merchant.new({
+  'name' => 'Cineworld'
+  })
+
+merchant_malones = Merchant.new({
+  'name' => 'Malones'
+  })
+
+merchant_tesco.save()
+merchant_malones.save()
+merchant_cineworld.save()
+
+
 
 
 ###CRUD tests here:
-
+# merchant_tesco.name = "TEST"
+# merchant_tesco.update() # Works
+# merchant_malones.delete() # Works
 
 # category_bills.name = "TEST"
 # category_bills.update() # Works
 # category_transport.delete() # Works
+
+
+# binding.pry
+# nil
---
+++ models/merchant.rb
@@ -0,0 +1,52 @@
+require_relative '../db/sql_runner.rb'
+
+
+class Merchant
+
+  attr_reader :id
+  attr_accessor :name
+
+  def initialize(options)
+    @id = options['id'].to_i() if options['id']
+    @name = options['name']
+  end
+
+  def save()
+    sql = "INSERT INTO merchants (name) VALUES ($1) RETURNING id"
+    values = [@name]
+    result = SqlRunner.run(sql, values).first
+    @id = result['id'].to_i()
+  end
+
+  def self.all()
+    sql = "SELECT * FROM merchants"
+    result = SqlRunner.run(sql)
+    return result.map { |merchant| Category.new(merchant)  }
+  end
+
+  def update()
+    sql = "UPDATE merchants SET name = $1 WHERE id = $2"
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM merchants WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM merchants"
+    SqlRunner.run(sql)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM merchants WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values).first
+    return Category.new(result)
+  end
+
+
+end
